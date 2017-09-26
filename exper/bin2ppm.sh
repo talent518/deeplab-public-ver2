9e96de1fc38168a968b7828ca13c05029301d25f
ZhangBaoCai <talent518@yeah.net>
2017-09-26 18:10:17 +0800

    优化bin2ppm.sh脚本，已执行过的文件直接跳过。

--- exper/bin2ppm.sh
+++ exper/bin2ppm.sh
@@ -1,1 +1,1 @@
-ls -1 */res/features/*/val/*/*/*.bin | awk '{a=substr($1, 1, length($1)-4) ".ppm";print a;system(" ../densecrf/prog_bin2ppm " $1 " " a);}'
+ls -1 */res/features/*/val/*/*/*.bin | awk '{a=substr($1, 1, length($1)-4) ".ppm";print a;system("test ! -f " a " && ../densecrf/prog_bin2ppm " $1 " " a);}'
